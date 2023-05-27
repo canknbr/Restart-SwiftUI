15c3e1d020ad93e2f5cf2648273d958475159e6a
canknbr <cankanbur1@gmail.com>
2023-05-27 13:13:29 +0300

    Initial Commit

---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by Can Kanbur on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
